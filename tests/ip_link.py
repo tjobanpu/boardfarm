b614f09bfb4096bce2f2f7100220de2fb0dc62e6
Nikhil Zinjurde <nikhil.zinjurde@imgtec.com>
2017-03-07 17:04:52 +0530

    tests: update InterfacesShow test to check total UP interfaces
    Signed-off-by: Nikhil Zinjurde <nikhil.zinjurde@imgtec.com>

--- tests/ip_link.py
+++ tests/ip_link.py
@@ -7,7 +7,7 @@
 
 import re
 import rootfs_boot
-from devices import board, wan, lan, wlan, prompt
+from devices import board, prompt
 
 class InterfacesShow(rootfs_boot.RootFSBootTest):
     '''Used "ip" or "ifconfig" to list interfaces.'''
@@ -17,14 +17,15 @@ class InterfacesShow(rootfs_boot.RootFSBootTest):
         board.expect(prompt)
         if "ip: not found" not in board.before:
             up_interfaces = re.findall('\d: ([A-Za-z0-9-\.]+)[:@].*state UP ', board.before)
+            ether_interfaces = re.findall('\d: ([A-Za-z0-9-\.]+)[:@].*\n.*link/ether ', board.before)
+            assert up_interfaces == ether_interfaces
         else:
             board.sendline('ifconfig')
             board.expect(prompt)
             up_interfaces = re.findall('([A-Za-z0-9-\.]+)\s+Link', board.before)
         num_up = len(up_interfaces)
-        if num_up >= 1:
-            msg = "%s interfaces are UP: %s." % (num_up, ", ".join(sorted(up_interfaces)))
+        if num_up:
+            self.result_message = "%s interfaces are UP: %s" % (num_up, ", ".join(sorted(up_interfaces)))
         else:
-            msg = "0 interfaces are UP."
-        self.result_message = msg
+            self.result_message = "0 interfaces are UP"
         assert num_up >= 1
